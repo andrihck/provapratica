b87abfbba37512e4860ce2b41f239294b9eb68a1
ANDRIELLI DOS SANTOS HENCKER <andrielli_hencker@estudante.sesisenai.org.br>
2024-07-04 11:36:09 -0300

    first commit

---
+++ provapratica/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/Header'
+import Clubes from './components/Clubes'
+
+function App(){
+  return(
+    <div>
+    <Header/>
+    <Clubes/>
+    </div>
+  )
+}
+export default App
---
+++ provapratica/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <img src="./senai.png" alt="" />
+            <h1>Andrielli dos Santos Hencker, 3c</h1>
+            </header>
+
+    )
+}
